e5c48bb94f6e0f6c7ebafacea117ab2d5286b07b
DimitarAndreev <dimiter.andreev@iesjoandaustria.org>
2019-05-22 22:48:14 +0200

    Exercici 4. Preparant el terreny: control de versions

---
+++ src/Vehicle.java
@@ -0,0 +1,42 @@
+public class Vehicle {
+
+    private static String model;
+    private static String marka;
+    private static String categoria;
+
+    public static final String BASIC = "BASIC";
+    public static final String LUXE = "LUXE";
+    public static final String GENERAL = "GENERAL";
+
+
+
+    public Vehicle(String model, String marka,String categoria) {
+        this.model = model;
+        this.marka = marka;
+        this.categoria=categoria;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMarca() {
+        return this.marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public static String getCategoria() {
+        return categoria;
+    }
+
+    public static void setCategoria(String categoria) {
+        Vehicle.categoria = categoria;
+    }
+}
